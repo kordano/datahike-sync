b0fd66b1b2e480731de07c36b5bd40766c989561
Konrad Kühne <konnykuehne@gmail.com>
2018-09-17 11:32:03 +0200

    add sync example

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject datahike-sync "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Synchronization example for datahike with p2p project dat"
+  :url "http://konradkuehne.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [io.replikativ/datahike "0.1.2"]
+                 [juxt/dirwatch "0.2.3"]])
--- src/datahike_sync/core.clj
+++ src/datahike_sync/core.clj
@@ -1,6 +1,86 @@
-(ns datahike-sync.core)
+(ns datahike-sync.core
+  (:require [datahike.api :as d]
+            [juxt.dirwatch :refer [watch-dir] :as dw]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def master-dir "/tmp/master-dat")
+(def master-uri (str "datahike:file://" master-dir))
+
+(comment
+  ;; cleanup database if necessary
+  (d/delete-database master-uri)
+
+  ;; create a database at this place
+  (d/create-database master-uri)
+  )
+
+(def master-conn (d/connect master-uri))
+
+(defn get-all-names [conn]
+  (d/q '[:find ?n
+         :where [?e :name ?n]]
+       @conn))
+
+;; lets add some data and wait for the transaction
+@(d/transact master-conn [{:db/id 1 :name "Alice" :age 33}
+                          {:db/id 2 :name "Bob" :age 37}
+                          {:db/id 3 :name "Charlie" :age 55}])
+
+(get-all-names master-conn)
+;; => #{["Charlie"] ["Alice"] ["Bob"]}
+
+(def slave-dir "/tmp/slave-dat")
+(def slave-uri (str "datahike:file://" slave-dir))
+
+;; create a dat in the master-dir directory and share it. Then clone the dat to the slave-dir directory. This should take a second or two.
+
+;; connect to cloned connection
+(def slave-conn (d/connect slave-uri))
+
+;; let's check the cloned data
+(get-all-names slave-conn)
+;; => #{["Charlie"] ["Alice"] ["Bob"]}
+
+
+;; add new data to the master database
+@(d/transact master-conn [{:db/id 4 :name "Dorothy"}])
+
+;; check new data in the master
+(get-all-names master-conn)
+;; => #{["Charlie"] ["Dorothy"] ["Alice"] ["Bob"]}
+
+
+;; check new data in the slave
+(get-all-names slave-conn)
+;; => #{["Charlie"] ["Alice"] ["Bob"]}
+
+;; reconnect slave
+(def slave-conn (d/connect slave-uri))
+
+;; check slave again
+(get-all-names slave-conn)
+;; => #{["Charlie"] ["Dorothy"] ["Alice"] ["Bob"]}
+
+;; create slave state
+(def slave-state (atom {:conn (d/connect slave-uri) :watcher nil}))
+(def slave-meta-dir (clojure.java.io/file (str slave-dir "/meta")))
+
+;; add file watcher function
+(defn on-meta-change [state file]
+  (prn "File changed:" (.getPath (:file file)))
+  (swap! state assoc :conn (d/connect slave-uri)))
+
+;; add watcher to file changes
+(swap! slave-state assoc :watcher (watch-dir
+                                   (partial on-meta-change slave-state)
+                                   slave-meta-dir))
+
+;; add new data to master and check it
+@(d/transact master-conn [{:db/id 5 :name "Eve"}])
+
+(get-all-names master-conn)
+;; => #{["Charlie"] ["Dorothy"] ["Alice"] ["Eve"] ["Bob"]}
+
+;; check slave state connection
+(get-all-names (:conn @slave-state))
+;; => #{["Charlie"] ["Dorothy"] ["Alice"] ["Eve"] ["Bob"]}
